edd009888ab86cfb84f951bf7de9c3162e0ec4cf
Roland Szabó <roland@rolandszabo.com>
2018-02-27 22:40:40 +0100

    Add a compile error

--- src/main/java/hu/elte/eserial/exception/EserialException.java
+++ src/main/java/hu/elte/eserial/exception/EserialException.java
@@ -4,7 +4,7 @@ package hu.elte.eserial.exception;
  * EserialException is the superclass of those exceptions that can be thrown during
  * serialization or deserialization with Eserial.
  */
-public class EserialException extends RuntimeException {
+public class EserialException extends RutimeException {
 
     public EserialException(String message) {
         super(message);
