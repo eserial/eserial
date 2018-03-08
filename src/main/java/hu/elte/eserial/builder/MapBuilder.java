275d0f9ce0e74efb582e020be17c6fb6bd9f3d2e
hegedusadam <had196@inf.elte.hu>
2018-03-08 15:44:18 +0100

    Create MapBuilder class, which builds the appropriate map for the object.

---
+++ src/main/java/hu/elte/eserial/builder/MapBuilder.java
@@ -0,0 +1,31 @@
+package hu.elte.eserial.builder;
+
+import hu.elte.eserial.exception.EserialException;
+import hu.elte.eserial.util.TypeUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class MapBuilder {
+
+    public static Map<Object, Object> build(Object value, Class<?> type) {
+        if (!TypeUtils.isMap(type) || !TypeUtils.isMap(value.getClass())) {
+            throw new EserialException("Type mismatch");
+        }
+
+        Map<Object, Object> map = (Map<Object, Object>) value;
+
+        if (TypeUtils.isConcurrentNavigableMap(type)) {
+            return new ConcurrentSkipListMap(map);
+        } else if (TypeUtils.isConcurrentMap(type)) {
+            return new ConcurrentHashMap(map);
+        } else if (TypeUtils.isSortedMap(type)) {
+            return new TreeMap(map);
+        } else {
+            return new HashMap(map);
+        }
+    }
+}
