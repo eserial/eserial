aaa85eef16f5c4338e35b3b5fc5b5445d2e70163
neuekaroly <karolyneue6@gmail.com>
2018-03-12 16:27:47 +0100

    Add BooleanParser and StringParsers to the parser package.

---
+++ src/test/java/hu/elte/eserial/parser/StringParserTest.java
@@ -0,0 +1,18 @@
+package hu.elte.eserial.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+
+public class StringParserTest {
+    @Test
+    public void parser_GiveASimpleString() {
+       Map<String, Object> testMap = new StringParser().parser("{\"car\":\"Audi\"}");
+
+       Assert.assertTrue(testMap.containsKey("car"));
+       Assert.assertTrue(testMap.containsValue("Audi"));
+       Assert.assertTrue(1 == testMap.size());
+    }
+}
