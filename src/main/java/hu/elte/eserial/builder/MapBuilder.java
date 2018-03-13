73d9ad91a9ebf6ffcf82d1643d12992e5544a0d2
hegedusadam <had196@inf.elte.hu>
2018-03-13 23:40:34 +0100

    Minor fix in CollectionBuilder, MapBuilder now handles interfaces and instantiated type differently.

--- src/main/java/hu/elte/eserial/builder/MapBuilder.java
+++ src/main/java/hu/elte/eserial/builder/MapBuilder.java
@@ -3,6 +3,7 @@ package hu.elte.eserial.builder;
 import hu.elte.eserial.exception.EserialException;
 import hu.elte.eserial.util.TypeUtils;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -11,21 +12,28 @@ import java.util.concurrent.ConcurrentSkipListMap;
 
 public class MapBuilder {
 
-    public static Map<Object, Object> build(Object value, Class<?> type) {
+    public static Map<Object, Object> build(Object value, Class type) throws Exception{
         if (!TypeUtils.isMap(type) || !TypeUtils.isMap(value.getClass())) {
             throw new EserialException("Type mismatch");
         }
 
         Map<Object, Object> map = (Map<Object, Object>) value;
 
-        if (TypeUtils.isConcurrentNavigableMap(type)) {
-            return new ConcurrentSkipListMap(map);
-        } else if (TypeUtils.isConcurrentMap(type)) {
-            return new ConcurrentHashMap(map);
-        } else if (TypeUtils.isSortedMap(type)) {
-            return new TreeMap(map);
-        } else {
-            return new HashMap(map);
+        if(type.isInterface()) {
+            if (TypeUtils.isConcurrentNavigableMap(type)) {
+                return new ConcurrentSkipListMap(map);
+            } else if (TypeUtils.isConcurrentMap(type)) {
+                return new ConcurrentHashMap(map);
+            } else if (TypeUtils.isSortedMap(type)) {
+                return new TreeMap(map);
+            } else {
+                return new HashMap(map);
+            }
         }
+
+        Map mapObject = (Map) type.newInstance();
+        mapObject.putAll(map);
+
+        return mapObject;
     }
 }
