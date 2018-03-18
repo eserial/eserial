eadca20676ccc20089ab5d477784d207fd5ef301
hegedusadam <had196@inf.elte.hu>
2018-03-18 14:49:07 +0100

    Create BuilderFactoryTests, implemented test methods.

---
+++ src/test/java/hu/elte/eserial/builder/BuilderFactoryTest.java
@@ -0,0 +1,45 @@
+package hu.elte.eserial.builder;
+
+import hu.elte.eserial.recursion.model.EserialElement;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertTrue;
+
+public class BuilderFactoryTest {
+
+    @Test
+    public void create_GivenSimpleType_ReturnsSimpleBuilder() {
+        assertTrue(BuilderFactory.create(Integer.class) instanceof SimpleBuilder);
+    }
+
+    @Test
+    public void create_GivenCollectionType_ReturnsCollectionBuilder() {
+        assertTrue(BuilderFactory.create(ArrayList.class) instanceof CollectionBuilder);
+    }
+
+    @Test
+    public void create_GivenMapType_ReturnsMapBuilder() {
+        assertTrue(BuilderFactory.create(HashMap.class) instanceof MapBuilder);
+    }
+
+    enum TestEnum {}
+
+    @Test
+    public void create_GivenEnumType_ReturnsEnumBuilder() {
+        assertTrue(BuilderFactory.create(TestEnum.class) instanceof EnumBuilder);
+    }
+
+    @Test
+    public void create_GivenDateType_ReturnsDateBuilder() {
+        assertTrue(BuilderFactory.create(Date.class) instanceof DateBuilder);
+    }
+
+    @Test
+    public void create_GivenCompoundType_ReturnsCompoundBuilder() {
+        assertTrue(BuilderFactory.create(EserialElement.class) instanceof CompoundBuilder);
+    }
+}
