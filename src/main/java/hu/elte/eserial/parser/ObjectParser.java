d0d38ec00f3fbc67c8aa268edc683a23365cb5d5
neuekaroly <karolyneue6@gmail.com>
2018-03-21 14:28:15 +0100

    ObjectParser now work with compound objects

--- src/main/java/hu/elte/eserial/parser/ObjectParser.java
+++ src/main/java/hu/elte/eserial/parser/ObjectParser.java
@@ -6,15 +6,25 @@ import java.util.Map;
 /**
  * Parses objects.
  */
+
 public class ObjectParser {
     public Map<String, Object> parser(String json) {
         Map<String, Object> map = new HashMap<>();
 
         json = json.substring(1, json.length()-1);
 
+        System.out.println(json);
+
         for(String pair : json.split(",")) {
-            String[] parts = pair.split(":");
+            String[] parts = pair.split(":", 2);
+
             String key = parts[0].trim().replace("\"", "");
+
+            if(parts[1].trim().charAt(0) == '{') {
+                map.put(key, parser(parts[1].trim()));
+                continue;
+            }
+
             String value = parts[1].trim().replace("\"", "");
 
             if(isNumeric(value)) {
