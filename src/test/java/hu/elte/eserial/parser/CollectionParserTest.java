e05ab12b45b61d7e2bd720b5a7c4309a4edb51b3
neuekaroly <karolyneue6@gmail.com>
2018-04-11 14:02:42 +0200

    Refactor the CollectionParser and add some test cases

--- src/test/java/hu/elte/eserial/parser/CollectionParserTest.java
+++ src/test/java/hu/elte/eserial/parser/CollectionParserTest.java
@@ -3,14 +3,16 @@ package hu.elte.eserial.parser;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class CollectionParserTest {
     @Test
-    public void parser_GivenAJsonWhichRepresentAListOfNumbers_ReturnLongList() {
-        LinkedList<Object> testList = new CollectionParser("2, 3,4, 5 ").parser();
+    public void parser_GivenAJsonWhichRepresentsAListOfNumbers_ReturnLongList() {
+        LinkedList<Object> testList = new CollectionParser("[2, 3,4, 5 ]").parser();
 
-        Assert.assertTrue(4 == testList.size());
+        Assert.assertEquals(4, testList.size());
+
         Assert.assertEquals(testList.get(0), new Long(2));
         Assert.assertEquals(testList.get(1), new Long(3));
         Assert.assertEquals(testList.get(2), new Long(4));
@@ -18,10 +20,11 @@ public class CollectionParserTest {
     }
 
     @Test
-    public void parser_GivenAJsonWhichRepresentAListOfBooleans_ReturnBooleanList() {
-        LinkedList<Object> testList = new CollectionParser("true, false, true, false").parser();
+    public void parser_GivenAJsonWhichRepresentsAListOfBooleans_ReturnBooleanList() {
+        LinkedList<Object> testList = new CollectionParser("[true, false, true, false]").parser();
 
-        Assert.assertTrue(4 == testList.size());
+        Assert.assertEquals(4, testList.size());
+
         Assert.assertEquals(testList.get(0), true);
         Assert.assertEquals(testList.get(1), false);
         Assert.assertEquals(testList.get(2), true);
@@ -29,14 +32,34 @@ public class CollectionParserTest {
     }
 
     @Test
-    public void parser_GivenAJsonWhichRepresentAListOfStrings_ReturnStringList() {
-        LinkedList<Object> testList = new CollectionParser("\"test1 \", \"test2\", \"test3\", \"test4\"").parser();
+    public void parser_GivenAJsonWhichRepresentsAListOfStrings_ReturnStringList() {
+        LinkedList<Object> testList = new CollectionParser("[\"test1 \", \"test2\", \"test3\", \"test4\"]").parser();
 
-        Assert.assertTrue(4 == testList.size());
+        Assert.assertEquals(4, testList.size());
 
         Assert.assertEquals(testList.get(0), "test1 ");
         Assert.assertEquals(testList.get(1), "test2");
         Assert.assertEquals(testList.get(2), "test3");
         Assert.assertEquals(testList.get(3), "test4");
+    }
+
+    @Test
+    public void parser_GivenAJsonWhichRepresentsAListOfObjects_ReturnObjectList() {
+        LinkedList<Object> testList = new CollectionParser("[ {\"key1\" : \"value1\", \"key2\" : \"value2\"}, {\"key1\" : \"value1\", \"key2\" : \"value2\"}]").parser();
+
+        Assert.assertTrue(2 == testList.size());
+
+        Assert.assertEquals(testList.get(0).getClass(), HashMap.class);
+        Assert.assertEquals(testList.get(1).getClass(), HashMap.class);
+    }
+
+    @Test
+    public void parser_GivenAJsonWhichRepresentsAListOfLists_ReturnListList() {
+        LinkedList<Object> testList = new CollectionParser("[ [\"key1\", \"value1\", \"key2\", \"value2\"], [\"key1\" , \"value1\", \"key2\" , \"value2\"]]").parser();
+
+        Assert.assertTrue(2 == testList.size());
+
+        Assert.assertEquals(testList.get(0).getClass(), LinkedList.class);
+        Assert.assertEquals(testList.get(1).getClass(), LinkedList.class);
     }
 }
