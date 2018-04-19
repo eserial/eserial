398ecf55fddb83a1403a62c40f03d86b583aeded
hegedusadam <had196@inf.elte.hu>
2018-04-19 10:28:51 +0200

    Create CollectionFactory, use it in CollectionBuilder.

---
+++ src/main/java/hu/elte/eserial/builder/CollectionFactory.java
@@ -0,0 +1,52 @@
+package hu.elte.eserial.builder;
+
+import hu.elte.eserial.util.TypeUtils;
+
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * Provides an adequate {@link java.util.Collection} implementation for a given {@link Type}.
+ */
+public class CollectionFactory {
+
+    /**
+     * Prevents the accidental instantiation of this factory class.
+     */
+    private CollectionFactory() {}
+
+    /**
+     * Creates a type-specific object builder instance.
+     *
+     * @param collectionClass
+     * @return an arbitrary {@link Class}
+     * @throws InstantiationException if could not instantiate the non-interface {@link Collection}
+     * @throws IllegalAccessException if could not instantiate the non-interface {@link Collection}
+     */
+    public static Collection create(Class collectionClass) throws InstantiationException, IllegalAccessException {
+        Collection collectionObject;
+
+        if (collectionClass.isInterface()) {
+            if (TypeUtils.isAssignableFrom(collectionClass, SortedSet.class)) {
+                collectionObject = new TreeSet<>();
+            } else if (TypeUtils.isAssignableFrom(collectionClass, Set.class)) {
+                collectionObject = new HashSet<>();
+            } else if (TypeUtils.isAssignableFrom(collectionClass, TransferQueue.class)) {
+                collectionObject = new LinkedTransferQueue();
+            } else if (TypeUtils.isAssignableFrom(collectionClass, BlockingDeque.class)) {
+                collectionObject = new LinkedBlockingDeque();
+            } else if (TypeUtils.isAssignableFrom(collectionClass, BlockingQueue.class)) {
+                collectionObject = new PriorityBlockingQueue();
+            } else if (TypeUtils.isAssignableFrom(collectionClass, Queue.class)) {
+                collectionObject = new ArrayDeque<>();
+            } else {
+                collectionObject = new ArrayList<>();
+            }
+        } else {
+            collectionObject = (Collection) collectionClass.newInstance();
+        }
+
+        return collectionObject;
+    }
+}
